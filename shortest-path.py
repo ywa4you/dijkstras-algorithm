c5b9446573fd77b13ef03b1f0a97f60ea151d342
ywa <5ab81c2babf2aabc9985122d2ef11aaf@tutanota.com>
2022-01-15 02:25:18 +0100

    created a basic idea of how the program would look, about 70% done (prior to optimisation)

--- shortest-path.py
+++ shortest-path.py
@@ -1,27 +1,64 @@
-# shortest path inpired by dijkstra
+# Implementation of Dijbstra's algorithm by ywa. This was initially
+# made as a part of a challenge.
+# Copyright (C) 2021 ywa
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import math
+
+# ywa's dijkstra's algorithm implementaition
 
 # graph in lists
 # god kill me please
-A = [["C", "D"], [7, 9]]
-B = [["C", "D"], [5, 3]]
-C = [["A", "B"], [7, 5]]
-D = [["A", "B"], [9, 3]]
-#first point
-crnt = A
+graf = [[[2, 3], [7, 9]],
+        [[2, 3], [5, 3]],
+        [[0, 1], [7, 5]],
+        [[0, 1], [9, 3]]]
 
 # temp vars for 
-unvisited = [A, B, C, D]
-shortest = ["1", "2", "3", "4"]
+visited = []
+shortest  = [0, math.inf, math.inf, math.inf]
+x = shortest.copy()
 
 
-def short_pnt(point):
-    short_val = min(point[1])
-    return point[0][point[1].index(short_val)], short_val
+def ch(point, way, point_val):
+    global shortest
+    if graf[point][1][way] + point_val < shortest[point]:
+        shortest[graf[point][0][way]] = graf[point][1][way]
+
+
+def da_way():
+    global x
+    for i in range(4):
+        y = min(x)
+# TODO fix this: index of point is in visited not its value
+        print(y in visited)
+        print(y)
+        print(visited)
+        if y in visited and y in x:
+            print("xd")
+            x.remove(y)
+        else:
+            #print(min(x))
+            #print(x)
+            return shortest.index(min(x))
 
 
 # body
-
 for i in range(4):
-    pnt,val = short_pnt(crnt)
-    unvisited.remove(pnt)
-    
+    cnt = da_way()
+    for j in range(2):
+        ch(cnt, j, shortest[cnt])
+    visited.append(cnt)
